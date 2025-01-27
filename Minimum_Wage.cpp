c1178d5d3b8c8ee7687b0c7bea8a657379ea7e8b
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-01-27 22:01:05 +0300

    Add files via upload
    contest2

---
+++ Minimum_Wage.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int x;
+  cin >> x;
+  if (x>11)
+  {
+    cout << "YES" << endl;
+  }
+  else
+  {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
