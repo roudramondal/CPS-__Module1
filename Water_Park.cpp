92bee1800e448b47146fc0be13d42ae1a9aa451d
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-01-29 05:26:28 +0300

    Add files via upload
    contest2

---
+++ Water_Park.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int W, H;
+    cin >> W >> H;
+    if (W >= 60 && H <= 130) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
