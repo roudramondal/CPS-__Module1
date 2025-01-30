beeffe46c7bd3a6a2a7b1f800bc7f99c0fee8832
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-01-30 20:49:25 +0300

    Add files via upload
    contest2

---
+++ Rivalry.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int r1, r2, d1, d2;
+  cin >> r1 >> r2 >> d1 >> d2;
+  int value1 = r1+d1;
+  int value2 = r2+d2;
+  
+  if (value1>value2)
+  {
+    cout << "Dominater" << endl;
+  }
+  else 
+  {
+    cout << "Everule" << endl;
+  }
+  return 0;
+}
