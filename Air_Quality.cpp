835f5f60e84b826b8d0c1bf1083aafd37f0a280c
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-01-31 19:06:14 +0300

    Add files via upload
    contest2

---
+++ Air_Quality.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int x;
+  cin >> x;
+  if (x>=1&&x<100)
+  {
+    cout << "YES" << endl;
+  }
+  else 
+  {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
