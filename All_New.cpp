5ecf81fcab9960ac64657d9d9c62afb3d03e922a
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-01-31 20:46:01 +0300

    Add files via upload
    contest2

---
+++ All_New.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int x, y;
+  cin >> x >> y;
+  if (x>y)
+  {
+    cout << "New" << endl;
+  }
+  else if (x<y)
+  {
+    cout << "Old" << endl;
+  }
+  else
+  {
+    cout << "Same" << endl;
+  }
+  return 0;
+}
