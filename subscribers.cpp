9130b75dfc7ea9d1b2fceacef8c3294309a2f2b1
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-02-01 20:59:40 +0300

    Add files via upload
    contest2

---
+++ subscribers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    if (N < 1000) {
+        cout << N << endl;
+    } else if (N < 10000) {
+        cout << (N / 10) * 10 << endl;
+    } else if (N < 100000) {
+        cout << (N / 100) * 100 << endl;
+    } else if (N < 1000000) {
+        cout << (N / 1000) * 1000 << endl;
+    } else if (N < 10000000) {
+        cout << (N / 10000) * 10000 << endl;
+    } else if (N < 100000000) {
+        cout << (N / 100000) * 100000 << endl;
+    } else if (N < 1000000000) {
+        cout << (N / 1000000) * 1000000 << endl;
+    }
+    
+    return 0;
+}
+
+
