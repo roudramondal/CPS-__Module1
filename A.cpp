dcc37f63f9f16aa3038dd00582b57dcd1f8d2981
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-02-09 19:12:14 +0300

    Add files via upload
    contest3

---
+++ A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int x;
+    cin >> x;
+    int sum = x - 10;
+    cout << sum << endl;
+  }
+  return 0;
+}
