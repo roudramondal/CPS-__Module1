cc4da84590be555655aab7e2bafbb9f52b1e37b6
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-02-13 06:51:32 +0300

    Add files via upload
    contest3

---
+++ O.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int s;
+    cin >> s;
+    int sum = 24 * s * 1000;
+    cout << sum << endl;
+  }
+  return 0;
+}
