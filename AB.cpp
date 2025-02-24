4181a7f1f627ec1390a29dae98c9bc15e8d84c08
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-02-25 00:37:58 +0600

    Add files via upload
    contest3

---
+++ AB.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<limits.h>
+using namespace std;
+int  main ()
+{
+  int n, m, k;
+  cin >> n >> k >> m;
+  int s = 0;
+  for (int i = 0; i < n - 1;i++)
+  {
+    int a;
+    cin >> a;
+    s += a;
+  }
+  int remaining = n * m - s;
+  if (remaining>k)
+  {
+    cout << "-1" << endl;
+  }
+  else 
+  {
+    cout << max(0, remaining) << endl;
+  }
+  return 0;
+}
