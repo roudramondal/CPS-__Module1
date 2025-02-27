5f4b1576cc735ddb44cf9fc1f9a37393ffd20477
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-02-27 19:29:12 +0600

    Add files via upload
    nestedloop

---
+++ Daimond.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int n;
+  cin >> n;
+  for (int i = 1; i < n;i++)// i<n 
+  {
+    for (int j = 1;j<=n-i;j++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= 2 * i - 1;j++)
+    {
+      cout<<"*";
+
+    }
+    cout << endl;
+  }
+  // inverse
+  for (int i = n; i >=1;i--)//n-1
+  {
+    for (int j = 1;j<=n-i;j++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= 2 * i - 1;j++)
+    {
+      cout<<"*";
+
+    }
+    cout << endl;
+  }
+}
