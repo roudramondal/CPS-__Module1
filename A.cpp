afef97cd4416fdbe7b0b4a3be595529f9052dac2
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-02-28 15:12:20 +0600

    Add files via upload
    contest4(array)basic

--- A.cpp
+++ A.cpp
@@ -1,15 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main ()
+int main()
 {
   int t;
   cin >> t;
+  
   while (t--)
   {
-    int x;
-    cin >> x;
-    int sum = x - 10;
-    cout << sum << endl;
+    int n;
+    cin >> n;
+    int count = 0;
+    for (int i = 1; i <= n;i++)
+    {
+      int a;
+      cin >> a;
+      if (a%2!=0)
+      {
+        count++;
+      }
+      else{
+
+      }
+    }
+    cout << count << endl;
   }
-  return 0;
+  
 }
