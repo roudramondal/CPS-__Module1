01e204c358a4dacb768f8f6f6acac44b8b2e6010
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-03-13 20:45:17 +0300

    Update Nine_array.cpp

--- Nine_array.cpp
+++ Nine_array.cpp
@@ -4,6 +4,7 @@ int main ()
 {
   int a, b;
   cin >> a >> b;
+  // Check if A and B are in the same row AND B is exactly one greater than A.
   if ((a+1==b)&&(a%3!=0))
   {
     cout << "Yes" << endl;
