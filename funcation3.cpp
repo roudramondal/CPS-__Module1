3663cfa1520a0b92d185f140415a476c6aef6de7
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-01 09:31:19 +0300

    Add files via upload
    swap two value between them using funaciton here i use address reference to swap that's it.

---
+++ funcation3.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int &a,int &b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+  return;
+}
+int main()
+{
+  int x, y;
+  cin >> x >> y;
+  swap(x, y);
+  cout << x <<" "<< y;
+  return 0;
+}
