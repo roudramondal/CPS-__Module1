e2cf4c19561cb0df22e677f0b265ad346069e2d2
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-01 08:39:35 +0300

    Add files via upload

---
+++ funcation1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum (int a,int b)
+{
+  int c=a+b;
+  return c;
+}
+int main()
+{
+  int x, y;
+  cin >> x >> y;
+  int z = sum(x, y);
+  cout << z << endl;
+  return 0;
+}
