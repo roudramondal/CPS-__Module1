8ff7b7ca8cdb6ec77ee67e13d2e1a760a5b1e23d
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-05 22:40:10 +0600

    Add files via upload
    i use here ceil() and floor function that's ceil mean's i some value is double (require decimal value) 8.3 ceil value is = 9 and floor value is = 8 print the value

---
+++ c3.cpp
@@ -0,0 +1,10 @@
+// use ceil(),floor()
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  double x = 9.2;
+  cout<<"ceil value = " << ceil(x) << endl;
+  cout<<"floor value = "<< floor(x) << endl;
+  return 0;
+}
