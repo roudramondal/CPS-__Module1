8376b95f4326721429533cebbc3fd38368a58df4
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-05 23:17:50 +0600

    Add files via upload
    Same way like is upper that's it

---
+++ lower.cpp
@@ -0,0 +1,17 @@
+// use islower()
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  char x;
+  cin >> x;
+  if (x>='a'&&x<='z')
+  {
+    cout << "value is lower" << endl;
+  }
+  else
+  {
+    cout << "Value is not lower" << endl;
+  }
+  return 0;
+}
