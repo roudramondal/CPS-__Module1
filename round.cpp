2e17b0dafa5ce887b5801908958b7f970be286c7
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-05 22:52:35 +0600

    Add files via upload
    I use here round function when I take input from the user, double value then i get for use round function nearest value that's it

---
+++ round.cpp
@@ -0,0 +1,11 @@
+// use  round function
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  double x;
+  cin >> x;
+  int final_value = round(x);
+  cout << final_value << endl;
+  return 0;
+}
