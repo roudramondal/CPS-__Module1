9875cd929d0e024145364a9201c83281ec9f5687
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-05 23:30:10 +0600

    Add files via upload
    Just find the digit that's it

---
+++ isdigit.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  char x;
+  cin >> x;
+  if (x>='0'&&x<='9')
+  {
+    cout << "true" << endl;
+  }
+  else
+  {
+    cout << "false" << endl;
+  }
+  return 0;
+}
