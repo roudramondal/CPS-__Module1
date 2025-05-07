afa6d78d69a8ed97fd376dd46747df0f3e1db3a7
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-07 19:11:28 +0600

    Add files via upload
    also this way can take input from the user

---
+++ vector_input.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  vector<int> v(5);
+  for (int i = 0; i < v.size();i++)
+  {
+    cin >> v[i];
+  }
+  for (int i = 0; i < v.size();i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+  return 0;
+}
