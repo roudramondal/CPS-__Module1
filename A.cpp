942e22a3c2973103044bcbf0947ced602820d7be
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-26 23:51:54 +0600

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
   int t;
-  cin >> t;
+  cin >> t;// test case input
   
   while (t--)
   {
-    int n;
+    int n;// input n
     cin >> n;
     int count = 0;
     for (int i = 1; i <= n;i++)
