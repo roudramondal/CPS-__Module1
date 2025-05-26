f18d8f851369ebc6a5499f5cff6d627f8157a8a3
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-26 23:53:30 +0600

    Update AD.cpp

--- AD.cpp
+++ AD.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main ()
 {
   int t;
-  cin >> t;
+  cin >> t;// input test case
   while (t--)
   {
     int n;
-    cin >> n;
+    cin >> n;// input taking 
     int max_quality = INT_MIN;
     int response_number = 0;
     for (int i = 1; i <= n;i++)
     {
-      int a, b;
+      int a, b;//  input value 
       cin >> a >> b;
       if (a>10)
       {
